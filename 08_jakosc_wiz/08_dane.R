e3a452f224f69d991243cd4cced67b4276dab8c9
lwawrowski <lukasz8989@gmail.com>
2018-11-26 12:32:22 +0100

    08 dane jakosciowe

--- 08_jakosc_wiz/08_dane.R
+++ 08_jakosc_wiz/08_dane.R
@@ -12,6 +12,7 @@ gss <- readRDS("08_jakosc_wiz/2016.rds")
 
 gss <- gss %>%
   select(vpsu, vstrat, polviews, born, adults, hompop, race, region, age, sex, one, wtssall) %>%
+  mutate_all(as.numeric) %>%
   mutate(polviews = 
            factor( polviews ,
                    labels = c( "Extremely liberal" , "Liberal" ,
@@ -30,7 +31,10 @@ gss <- gss %>%
                                "East North Central" , "West North Central" ,
                                "South Atlantic" , "East South Central" ,
                                "West South Central" , "Mountain" , "Pacific" )
-           ))
+           ),
+         sex=factor(sex, labels=c("Male", "Female")),
+         born_in_usa=factor(born_in_usa, labels = c("Yes", "No"))) %>%
+  select(-born)
 
 save(gss, file="08_jakosc_wiz/gss.RData")
 
