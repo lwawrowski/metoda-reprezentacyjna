303d4f49cabaaff97dfebc4da0cc601ae8264612
lwawrowski <lukasz8989@gmail.com>
2019-10-14 07:59:55 +0200

    projekty

--- z07_projekt/wyk_degurba.R
+++ z07_projekt/wyk_degurba.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(survey)
 
-load("07_projekt/dane.RData")
+load("dane.RData")
 
 # stworzenie warstwy
 
 populacja <- populacja %>%
-  mutate(warstwa1=paste0(woj,"_",degurba))
+  mutate(warstwa1=paste0(degurba))
 
 # losowanie próby 
 
@@ -40,19 +40,46 @@ proba1_badanie <- merge(proba1, badanie, by = "id_gospodarstwa", all.x = T)
 
 proba1_badanie_bez_brakow <- proba1_badanie %>%
   filter(!is.na(wiek)) %>%
-  mutate(bezr=ifelse(aktywnosc_ekon==2,1,0), 
-         mianownik=ifelse(aktywnosc_ekon %in% c(1,2),1,0))
+  mutate(wyk_sr=ifelse(wyksztalcenie==2, 1, 0),
+         mianownik=1)
 
 proba1_bez_brakow_schemat <- svydesign(ids = ~id_gospodarstwa, weights = ~waga, strata = ~warstwa1,
                                        fpc = ~n, data = proba1_badanie_bez_brakow)
 
-# kalibracja w ramach województw
+# kalibracja w ramach degurba
 
 pop_total <- colSums(model.matrix(~degurba,model.frame(~degurba,populacja)))
 
 proba1_kalibracja <- calibrate(design = proba1_bez_brakow_schemat, formula = ~degurba, population = pop_total)
 
-# estymacja stopy bezrobocia
+# estymacja średniej liczby osób w gosp. 
 
-stopa_bezr_degurba <- svyby(formula = ~bezr, denominator = ~mianownik, by = ~degurba, 
+srednia_wielkosc_gosp <- svyby(formula = ~liczba_osob, by = ~degurba, 
+                               design = proba1_kalibracja, FUN = svymean)
+
+# względny błąd oszacowania i przedziały ufności
+
+q <- qnorm(1-0.1/2)
+
+srednia_wielkosc_gosp <- srednia_wielkosc_gosp %>%
+  mutate(cv=se/liczba_osob*100,
+         dg=liczba_osob-q*se,
+         gg=liczba_osob+q*se)
+
+
+# estymacja odsetka średniego wykształcenia
+
+wyk_sr_degurba <- svyby(formula = ~wyk_sr, denominator = ~mianownik, by = ~degurba, 
                             design = proba1_kalibracja, FUN = svyratio)
+
+names(wyk_sr_degurba) <- c("degurba", "ods_wyk", "se")
+
+# względny błąd oszacowania i przedziały ufności
+
+q <- qnorm(1-0.1/2)
+
+wyk_sr_degurba <- wyk_sr_degurba %>%
+  mutate(cv=se/ods_wyk*100,
+         dg=ods_wyk-q*se,
+         gg=ods_wyk+q*se)
+
