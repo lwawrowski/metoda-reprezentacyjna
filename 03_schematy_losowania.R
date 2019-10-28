de38a1522132a153d10dc044e2c92cd3fea70f49
lwawrowski <lukasz8989@gmail.com>
2019-10-28 14:25:38 +0100

    schematy losowania - zadania

--- 03_schematy_losowania.R
+++ 03_schematy_losowania.R
@@ -40,6 +40,17 @@ data.frame(pik=pik,name=belgianmunicipalities$Commune)
 # the inclusion probability sum is equal to the sample size
 sum(pik)
 
+# proporcjonalne
+
+los_prop <- obwody %>%
+  mutate(p_prop=inclusionprobabilities(wyborcy, 1000)) %>%
+  sample_n(1000, weight = wyborcy)
+
+los_prop <- los_prop %>%
+  mutate(waga=(1/p_prop)*(1000/27408))
+
+sum(los_prop$waga)
+
 # warstwowe
 
 liczebnosci_warstwa <- rep(10, 41)
@@ -76,3 +87,31 @@ sejm <- obwody %>%
 los_warstwa1 <- strata(data = obwody, stratanames = c("numer_okregu_do_senatu"), size = sejm$proc)
 
 sum(1/los_warstwa1$Prob)
+
+# losowanie systematyczne
+
+proba2700 <- obwody %>%
+  mutate(prob=2700/nrow(.))
+
+UPsystematic(proba2700$prob)
+
+proba2700 <- proba2700 %>%
+  mutate(w_probie=UPsystematic(prob)) %>%
+  filter(w_probie == 1)
+
+# przystanki
+
+przystanki <- openPoznan::tram_bus_stops()
+
+przystanki <- przystanki %>%
+  mutate(Number_routes=length(str_split(string = Stop_Headsigns, pattern = ",")))
+
+length_colon <- function(x){
+  length(str_split(x,","))
+}
+
+for(i in 1:nrow(przystanki)){
+  przystanki$Number_routes[i] <- length(strsplit(as.character(przystanki$Stop_Headsigns[i]), ",")[[1]])
+}
+
+save(przystanki, file = "data/przystanki.RData")
